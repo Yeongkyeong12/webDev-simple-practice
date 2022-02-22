7e3361e4ef375c6bb21f5e5e64d81a5538bdf678
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-02-22 23:43:59 +0900

    Docs: practice with windowWidth

--- react/useeffect-practice/src/App.js
+++ react/useeffect-practice/src/App.js
@@ -1,25 +1,19 @@
 import React, { useState, useEffect } from "react";
 
 export default function App() {
-  const [resourceType, setResourceType] = useState("posts");
-  const [items, setItems] = useState([]);
+  const [windowWidth, setwindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setwindowWidth(window.innerWidth);
+  };
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then((response) => response.json())
-      .then((json) => setItems(json));
-  }, [resourceType]);
-  return (
-    <>
-      <div>
-        <button onClick={() => setResourceType("posts")}>Posts</button>
-        <button onClick={() => setResourceType("users")}>Users</button>
-        <button onClick={() => setResourceType("comments")}>Comments</button>
-      </div>
-      <h1>{resourceType}</h1>
-      {items.map((item) => {
-        return <pre>{JSON.stringify(item)}</pre>;
-      })}
-    </>
-  );
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return <div>{windowWidth}</div>;
 }
